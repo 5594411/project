3ac5381869752b9f3c63ff319766daf518276577
5594411 <z5594411@ad.unsw.edu.au>
2025-07-29 20:05:25 +1000

    Update sim file for swap

--- project.srcs/sources_1/new/swap.vhd
+++ project.srcs/sources_1/new/swap.vhd
@@ -3,7 +3,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity swap is
-    port (
+    port ( clk : in std_logic;
+        enable : in std_logic;
         block_x         : in  std_logic_vector(7 downto 0);
         block_y         : in  std_logic_vector(7 downto 0);
         px         : in  std_logic_vector(3 downto 0);
@@ -16,27 +17,29 @@ end swap;
 architecture behavioral of swap is
     constant BLOCK_WIDTH : integer := 8;
 begin
-    swap_proc: process(block_x, block_y, px, py, s)
-        variable temp_bx, id_x : std_logic_vector(BLOCK_WIDTH - 1 downto 0);
-        variable temp_by, id_y : std_logic_vector(BLOCK_WIDTH - 1 downto 0);
+    process(clk)
+        variable temp_bx : std_logic_vector(BLOCK_WIDTH - 1 downto 0);
+        variable temp_by : std_logic_vector(BLOCK_WIDTH - 1 downto 0);
         variable temp_bit : std_logic;
 
-        variable idx_x, idx_y : integer;
+        variable id_x, id_y : integer;
 
     begin
-        temp_bx := block_x;
-        temp_by := block_y;
-
-        for i in 0 to to_integer(unsigned(s)) - 1 loop
-            idx_x := (to_integer(unsigned(px)) + i) mod BLOCK_WIDTH;
-            idx_y := (to_integer(unsigned(py)) + i) mod BLOCK_WIDTH;
-            temp_bit := temp_bx(idx_x);
-            temp_bx(idx_x) := temp_by(idx_y);
-            temp_by(idx_y) := temp_bit;
-        end loop;
-
-        bx_swapped <= temp_bx;
-        by_swapped <= temp_by;
-
-    end process swap_proc;
+        if rising_edge(clk) AND enable = '1' then
+            temp_bx := block_x;
+            temp_by := block_y;
+    
+            for i in 0 to to_integer(unsigned(s)) - 1 loop
+                id_x := (to_integer(unsigned(px)) + i) mod BLOCK_WIDTH;
+                id_y := (to_integer(unsigned(py)) + i) mod BLOCK_WIDTH;
+                
+                temp_bit := temp_bx(id_x);
+                temp_bx(id_x) := temp_by(id_y);
+                temp_by(id_y) := temp_bit;
+            end loop;
+    
+            bx_swapped <= temp_bx;
+            by_swapped <= temp_by;
+        end if;
+    end process;
 end behavioral;
