00d67c3256c6ece7cd852bc18921e02074a0c8e6
AlexandraUkrainskaya <archist250505@gmail.com>
2025-07-29 18:30:41 +1000

    Update shifter.vhd
    not yet tested

--- project.srcs/sources_1/new/shifter.vhd
+++ project.srcs/sources_1/new/shifter.vhd
@@ -26,16 +26,19 @@ use IEEE.NUMERIC_STD.ALL;
 entity shifter is
     port (
         s_b        : in  std_logic_vector(7 downto 0);
-        block_size, r : in integer range 1 to 8;
+        block_size_in, r_in : std_logic_vector(3 downto 0);
         shift_o    : out std_logic_vector(7 downto 0)
     );
 end entity shifter;
 architecture Behavioral of shifter is
-    signal block_end, modulo : integer range 0 to 8;
+    signal block_end, modulo, block_size, r : integer range 0 to 8;
 begin
-    --shift_o(8 - block_size downto 0) <= (OTHERS => '0');
-    modulo <= r mod block_size;
-    block_end <= 8 - block_size;
+    --case: rotate by 4 block size 
+    block_size <= to_integer(unsigned(block_size_in));
+    r <= to_integer(unsigned(r_in));
+    modulo <= r mod block_size;  --modulo = 4
+    
+    block_end <= 8 - block_size; --it is 1
     process(block_size, modulo, s_b) begin
         if (block_size = 1) then --bo change as there is only 1 bit
             shift_o <= s_b;
@@ -45,7 +48,8 @@ begin
             elsif (modulo = block_size - 1) then
                 shift_o(7 downto block_end) <= s_b(block_end) & s_b(7 downto block_end + 1);
             else
-                shift_o(7 downto block_end) <= s_b(7 - modulo downto block_size) & s_b(7 downto 7 - modulo + 1);
+                --7 downto 1 is 3 downto 1 & 7 downto 4
+                shift_o(7 downto block_end) <= s_b(7 - modulo downto block_end) & s_b(7 downto 7 - modulo + 1);
             end if;
         end if;
     end process;
